7f6e951e5bdf1b1d57f7f9d020d1e524040a71c9
wtrnash <396612789@qq.com>
2018-07-30 11:02:40 +0800

    python 两数之和

---
+++ python/001两数之和/001两数之和.py
@@ -0,0 +1,33 @@
+"""
+题目：
+给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+"""
+"""
+解答：
+二重循环遍历list，第一重循环遍历每个元素时，同时遍历之后的每个元素，进行相加，和目标进行判等
+"""
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        length = len(nums)
+        for i in range(length):
+            for j in range(i + 1, length):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
+solution = Solution()
+answer = solution.twoSum([2,7,11,12], 9)
+print(answer)
