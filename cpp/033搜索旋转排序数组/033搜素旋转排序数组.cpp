c2285db76b744b886346f94a1ec26a8820423cb1
wtrnash <396612789@qq.com>
2018-12-03 10:14:50 +0800

    033cpp

---
+++ cpp/033搜索旋转排序数组/033搜素旋转排序数组.cpp
@@ -0,0 +1,67 @@
+/*
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+
+你可以假设数组中不存在重复的元素。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+
+示例 1:
+
+输入: nums = [4,5,6,7,0,1,2], target = 0
+输出: 4
+示例 2:
+
+输入: nums = [4,5,6,7,0,1,2], target = 3
+输出: -1
+*/
+
+//解答：还是用二分搜索来做，第一次二分后，左右两边一定有一个是有序的，mid的值小于high的值，则右边有序，判断target在不在这之中
+//mid值大于low的值则左边有序，判断target在不在这之中，以此类推即可
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size() == 0)
+            return -1;
+        
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
+        while(low <= high)
+        {
+            mid = (low + high) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < nums[high]) //右边有序
+            {
+                if(target > nums[mid] && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            else    //左边有序
+            {
+                 if(target < nums[mid] && target >= nums[low])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
