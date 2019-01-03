46c0a6276deb71348924e8ef0e11a48917952ccd
wtrnash <396612789@qq.com>
2019-01-03 09:59:28 +0800

    058py

---
+++ python/058最后一个单词的长度/058最后一个单词的长度.py
@@ -0,0 +1,33 @@
+"""
+给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+
+如果不存在最后一个单词，请返回 0 。
+
+说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+
+示例:
+
+输入: "Hello World"
+输出: 5
+"""
+
+# 简单题。从字符串最后开始往前遍历，遇到空格不计数。遇到第一个单词开始计数，一直到再次遇到空格后跳出返回结果
+# 这题提交成功率低的原因应该还是在理解题意上，有人可能会认为如果从后往前遍历遇到第一个空格那么直接返回0
+# 但实际上应该遍历到遇到第一个单词再计数
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        n = len(s) - 1
+        for i in range(n, -1, -1):
+            if count == 0 and s[i] == ' ':
+                continue
+            elif not s[i] == ' ':
+                count = count + 1
+            else:
+                break
+        
+        return count
