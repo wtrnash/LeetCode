6a1b3e99bfeac977f77ea733efc6dbd1d21ed8ef
wtrnash <wtrnash@foxmail.com>
2019-01-19 09:38:30 +0800

    074py

---
+++ python/074搜索二维矩阵/074搜索二维矩阵.py
@@ -0,0 +1,51 @@
+"""
+编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+
+每行中的整数从左到右按升序排列。
+每行的第一个整数大于前一行的最后一个整数。
+示例 1:
+
+输入:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+输出: true
+示例 2:
+
+输入:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+输出: false
+"""
+# 解答：可以以左上角为分界线，大于目标值，则将列不断减小，一直到0为止；如果小于目标值，则增大行，不断逼近。
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        if n == 0:
+            return False
+        i = 0
+        j = n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i = i + 1
+            else:
+                j = j - 1
+        
+        return False
