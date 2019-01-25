3261374d7788497fd48f38fd674ccc88d1ae3c4d
wtrnash <wtrnash@foxmail.com>
2019-01-25 12:12:28 +0800

    080py

---
+++ python/080删除排序数组中的重复项 II/080删除排序数组中的重复项 II.py
@@ -0,0 +1,37 @@
+"""
+给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+示例 1:
+
+给定 nums = [1,1,1,2,2,3],
+
+函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。
+
+你不需要考虑数组中超出新长度后面的元素。
+示例 2:
+
+给定 nums = [0,0,1,1,1,1,2,3,3],
+
+函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。
+
+你不需要考虑数组中超出新长度后面的元素。
+
+"""
+# 解答：用一个变量index记录当前新数组对应的下标。遍历原数组，如果当前的值和前两个的不相等，那就记录到index位置，index自增1。如果是重复的，则不对index进行改变
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return len(nums)
+        index = 0
+        for val in nums:
+            if index < 2 or not val == nums[index - 2]:
+                nums[index] = val
+                index += 1
+        
+        return index
