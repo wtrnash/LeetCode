61863be5be90fa3400b23b30c85358495bc0a392
wtrnash <wtrnash@foxmail.com>
2019-03-03 13:54:14 +0800

    117py

---
+++ python/117填充每个节点的下一个右侧节点指针 II/117填充每个节点的下一个右侧节点指针 II.py
@@ -0,0 +1,78 @@
+"""
+给定一个二叉树
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+ 
+
+示例：
+
+
+
+输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+
+输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ 
+
+提示：
+
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+
+"""
+
+# 解答：类似116，但要考虑左右子树为空的情况，分别做处理。这里用递归做。然后要注意先递归右边，连上右边的next，这样再左边时子树可以相连
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root == None:
+            return root
+        
+        if root.left == None and root.right == None:
+            return root
+        
+        q = root.next
+        temp = None
+        while q:
+            if q.left:
+                temp = q.left
+                break
+            elif q.right:
+                temp = q.right
+                break
+            else:
+                q = q.next
+        
+        
+        if root.left:
+            if root.right:
+                root.left.next = root.right
+            else:
+                root.left.next = temp
+        
+        if root.right:
+            root.right.next = temp
+            
+        
+        self.connect(root.right)
+        self.connect(root.left)
+        
+        return root
