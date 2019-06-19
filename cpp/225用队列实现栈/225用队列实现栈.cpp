8108ad26167fd294544daddea44bea21bb161513
wtrnash <wtrnash@foxmail.com>
2019-06-19 09:35:55 +0800

    225cpp

---
+++ cpp/225用队列实现栈/225用队列实现栈.cpp
@@ -0,0 +1,71 @@
+/*
+使用队列实现栈的下列操作：
+
+push(x) -- 元素 x 入栈
+pop() -- 移除栈顶元素
+top() -- 获取栈顶元素
+empty() -- 返回栈是否为空
+注意:
+
+你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的。
+你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。
+
+*/
+
+//解答：利用队列实现。push就还是队列的Push。pop就辅助再利用一个队列，弹出所有元素除了最后一个元素都加入另一个队列，再重新加回原来的队列
+#include <queue>
+using namespace std;
+class MyStack {
+public:
+    queue<int> q;
+    /** Initialize your data structure here. */
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        q.push(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int result;
+        queue<int> temp;
+        while(!q.empty())
+        {
+            result = q.front();
+            q.pop();
+            if(!q.empty())
+                temp.push(result);
+        }
+        
+        while(!temp.empty())
+        {
+            q.push(temp.front());
+            temp.pop();
+        }
+        
+        return result;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q.back();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
