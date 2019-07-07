2432dde713c05f5d15410f5c1c2c2bccb7339231
wtrnash <wtrnash@foxmail.com>
2019-07-07 10:27:24 +0800

    240py

---
+++ python/240搜索二维矩阵 II/240搜索二维矩阵 II.py
@@ -0,0 +1,46 @@
+"""
+编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+
+每行的元素从左到右升序排列。
+每列的元素从上到下升序排列。
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+"""
+
+# 解答：如果大于每行的最后一个，则一定在这行的之后，所以行数+=1，否则列数-=1
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        n = len(matrix)
+        if n == 0:
+            return False
+        m = len(matrix[0])
+        if m == 0:
+            return False
+        i = 0
+        j = m - 1
+        while i < n and j >= 0:
+            if target == matrix[i][j]:
+                return True
+            elif target > matrix[i][j]:
+                i += 1
+            else:
+                j -= 1
+        return False
